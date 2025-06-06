0506d446e54f98a5a7d2d7078d51d1b028fa83f5
Cody Flowers <c.j.flowers05@gmail.com>
2025-06-06 16:59:21 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'fit-tracker-cache-v1';
+const CACHE_NAME = "ethan-fit-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/styles.css',
-  '/scripts/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  "/",
+  "/index.html",
+  "/day.html",
+  "/goals.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
@@ -17,7 +17,7 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
